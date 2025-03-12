55b9801c36a6b3f4ec53732496fac02dce1fc732
JoaoVictor3DS <joaovictorpinheiro2007@gmail.com>
2025-03-12 11:53:50 -0300

    Translate to english

--- 25-03-12/SecretariaEscola/escola/models.py
+++ 25-03-12/SecretariaEscola/escola/models.py
@@ -16,16 +16,15 @@ class Guardian(models.Model):
         ("3C", "3° Ano C"),
     )
 
-    first_name = models.CharField(
-        max_length=50, verbose_name="Primeiro nome do responsável"
-    )
-    last_name = models.CharField(
-        max_length=50, verbose_name="Último nome do responsável"
+    first_name = models.CharField(max_length=50, verbose_name="Guardian's first name")
+    last_name = models.CharField(max_length=50, verbose_name="Guardian's last name")
+    registration_number_student = models.CharField(
+        max_length=6, unique=True, verbose_name="Student's registration"
     )
     phone_number = models.CharField(
-        max_length=15, verbose_name="N° do celular do responsável (xx) xxxxx-xxxx"
+        max_length=15, verbose_name="Guardian's phone number (xx) xxxxx-xxxx"
     )
-    email = models.EmailField(max_length=100, verbose_name="Email do responsável")
+    email = models.EmailField(max_length=100, verbose_name="Guardian's email")
     adress = models.CharField(max_length=100)
     cpf = models.CharField(max_length=11, unique=True)
     birthday = models.DateField()
@@ -50,12 +49,15 @@ class Student(models.Model):
         ("3C", "3° Ano C"),
     )
 
-    first_name = models.CharField(max_length=50, verbose_name="Primeiro nome do aluno")
-    last_name = models.CharField(max_length=50, verbose_name="Último nome do aluno")
-    phone_number = models.CharField(
-        max_length=15, verbose_name="N° do celular do aluno (xx) xxxxx-xxxx"
+    first_name = models.CharField(max_length=50, verbose_name="Student's first name")
+    last_name = models.CharField(max_length=50, verbose_name="Student's last name")
+    registration_number = models.CharField(
+        max_length=6, unique=True, verbose_name="Student's registration"
     )
-    email = models.EmailField(max_length=100, verbose_name="Email do aluno")
+    phone_number = models.CharField(
+        max_length=15, verbose_name="Student's phone number (xx) xxxxx-xxxx"
+    )
+    email = models.EmailField(max_length=100, verbose_name="Student's email")
     adress = models.CharField(max_length=100)
     cpf = models.CharField(max_length=11, unique=True)
     birthday = models.DateField()
@@ -80,14 +82,12 @@ class Professor(models.Model):
         ("3C", "3° Ano C"),
     )
 
-    first_name = models.CharField(
-        max_length=50, verbose_name="Primeiro nome do Professor"
-    )
-    last_name = models.CharField(max_length=50, verbose_name="Último nome do Professor")
+    first_name = models.CharField(max_length=50, verbose_name="Professor's first name")
+    last_name = models.CharField(max_length=50, verbose_name="Professor's last name")
     phone_number = models.CharField(
-        max_length=15, verbose_name="N° do celular do Professor (xx) xxxxx-xxxx"
+        max_length=15, verbose_name="Professor's phone number (xx) xxxxx-xxxx"
     )
-    email = models.EmailField(max_length=100, verbose_name="Email do Professor")
+    email = models.EmailField(max_length=100, verbose_name="Professor's Email")
     adress = models.CharField(max_length=100)
     cpf = models.CharField(max_length=11, unique=True)
     birthday = models.DateField()
