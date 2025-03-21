9c586cd269515f03219a982c7a32fbee8ab5c632
João Victor <joaovictorpinheiro2007@gmail.com>
2025-03-21 17:50:20 -0300

    Add school secretary as submodule

--- SecretariaEscola/SecretariaEscola/views.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponse
-
-def Home (resquest):
-    return HttpResponse('<h1>Home Page<h1/>')
--- SecretariaEscola/escola/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def Home(request):
-    return HttpResponse('<h1>Home Page of Escola<h1>')
